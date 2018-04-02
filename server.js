31dc96aa724730425fe133e28b0eb74eaff11a84
Cecily Grossmann <grssmnn2@gmail.com>
2018-04-01 19:22:26 -0500

    heroku push

--- server.js
+++ server.js
@@ -78,6 +78,7 @@ app.get("/articles", function (req, res) {
     .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
+      res.send(dbArticle);
     })
     .catch(function (err) {
       // If an error occurred, send it to the client
